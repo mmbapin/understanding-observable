49ac9ba03f7bf306863384e2730c74cf9873ebea
M. M. Bapin <m.bapin@konasl.com>
2023-12-12 10:00:38 +0600

    task done for 12 dec

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if(count > 3){
+          observer.error(new Error('Count is greater 3!'));
+        }
+        count++;
+      }, 1000)
+    })
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message)
+    })
   }
 
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe()
+  }
 }
